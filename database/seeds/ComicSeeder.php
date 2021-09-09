472d604db9213a4a94e08955f365c5b3d3b97a03
DamianoD88 <dispe88@live.it>
2021-09-09 18:33:51 +0200

    add first

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comic = new Comic();
+    }
+}
