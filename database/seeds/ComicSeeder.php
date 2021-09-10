58bb1512fc4c3f2d5bfab380a04c61bc089a4c8c
DamianoD88 <dispe88@live.it>
2021-09-10 16:47:54 +0200

    prova con ciclo for

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -12,16 +12,25 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        $comic = new Comic();
 
-        $comic->id = 1;
-        $comic->tipologia = "fantasy";
-        $comic->shop = "online";
-        $comic->characters = "Marvel";
-        $comic->tv = "k2";
-        $comic->videos = "10 episodi";
+        for($a = 0; $a < 10; $a++){
 
-        $comic->save();
+            $comic = new Comic();
+
+            // campo di incremento automatico stesso non deve essere specificato.
+            // $comic->id = 1;
+            $comic->tipologia = "fantasy";
+            $comic->shop = "online";
+            $comic->characters = "Marvel";
+            $comic->tv = "k2";
+            $comic->videos = "10 episodi";
+
+            $comic->save();
+
+
+        }
+
+        
         
     }
 }
